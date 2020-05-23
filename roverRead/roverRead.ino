138f48a96a2b58571d4c7478264f0a0f5d4a11b0
Allen Kreager <akreager87@gmail.com>
2020-05-23 13:51:20 -0400

    trying to read roboclaw data

--- roverRead/roverRead.ino
+++ roverRead/roverRead.ino
@@ -1,21 +1,32 @@
+//Sketch to read data from Roboclaw motordriver
+//Basicmicro Arduino library and examples:
+//https://github.com/basicmicro/roboclaw_arduino_library
+//Connections:
+//Roboclaw     Arduino Mega     Serial LCD
+//   S1          18, TX1
+//   S2          19, RX1
+//               16, TX2           RX
+
 #include "RoboClaw.h"
 
 RoboClaw roboclaw(&Serial1, 10000);
 
 #define address 0x80
 
+//Serial LCD commands
 #define COMMAND 0xFE
 #define CLEAR   0x01
 #define LINE0   0x80
 #define LINE1   0xC0
 
 int led = 13;
+unsigned int rawBattery = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
-  
+
   //Communciate with roboclaw at 230400 baud
   roboclaw.begin(230400);
   delay(100);
@@ -26,8 +37,8 @@ void setup() {
   delay(100);
 
   Serial2.write(COMMAND);
-  Serial2.write(LINE0);
-  Serial2.print(F(" Main Battery:"));
+  Serial2.write(LINE0 + 2);
+  Serial2.print(F("Main Battery:"));
 
   delay(1000);
 }
@@ -35,21 +46,24 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   bool valid1;
-  uint16_t rawBattery = roboclaw.ReadMainBatteryVoltage(address, &valid1);
-  //uint16_t rawBattery = roboclaw.ReadMainBatteryVoltage(address);
-  float mainBattery = rawBattery / 10.0;
+
+  rawBattery = roboclaw.ReadMainBatteryVoltage(address, &valid1);
+  int mainBattery = rawBattery / 10;
 
   if (valid1) {
     Serial2.write(COMMAND);
-    Serial2.write(LINE1);
+    Serial2.write(LINE1 + 5);
     Serial2.print(mainBattery);
-    Serial2.print(F(" V    "));
+    Serial2.print(F("."));
+    Serial2.print(rawBattery - (mainBattery * 10));
+    Serial2.print(F(" V   "));
   }
   else {
     Serial2.write(COMMAND);
-    Serial2.write(LINE1);
-    Serial2.print(F("    INVALID! "));
+    Serial2.write(LINE1 + 4);
+    Serial2.print(F("INVALID! "));
   }
+
   delay(1000);
 }
 
---
+++ roverReadVersion/roverReadVersion.ino
@@ -0,0 +1,39 @@
+//See BareMinimum example for a list of library functions
+
+//Includes required to use Roboclaw library
+#include "RoboClaw.h"
+
+RoboClaw roboclaw(&Serial1, 10000);
+
+#define address 0x80
+
+//Serial LCD commands
+#define COMMAND 0xFE
+#define CLEAR   0x01
+#define LINE0   0x80
+#define LINE1   0xC0
+
+void setup() {
+  //Communciate with roboclaw at 230400 baud
+  roboclaw.begin(230400);
+  delay(100);
+
+  //Communicate with serial LCD at 9600 baud
+  Serial2.begin(9600);
+  delay(100);
+  Serial2.write(COMMAND);
+  Serial2.write(CLEAR);
+  delay(100);
+}
+
+void loop() {
+  char version[32];
+
+  if (roboclaw.ReadVersion(address, version)) {
+    Serial2.write(COMMAND);
+    Serial2.write(LINE0);
+    Serial2.print(version);
+  }
+
+  delay(100);
+}
